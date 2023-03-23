44eb722041c88d3181f3da1977d0a77423f77f93
River <75063620+RvrCS@users.noreply.github.com>
2023-03-23 04:18:42 -0700

    Added Guests and Add guest activities

---
+++ app/src/main/java/gomez/abraham/organizadoreventos/AgregarInvitado.kt
@@ -0,0 +1,11 @@
+package gomez.abraham.organizadoreventos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarInvitado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_invitado)
+    }
+}
