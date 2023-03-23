e99df9a0aa7b78c5c5deeb260a5a547e0c1e918e
Christopher Kuraica <christopherkuraica@gmail.com>
2023-03-23 02:55:08 -0700

    Added Sign in and sign up activities

---
+++ app/src/main/java/gomez/abraham/organizadoreventos/RegistroActivity.kt
@@ -0,0 +1,55 @@
+package gomez.abraham.organizadoreventos
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        //Initialize UI elements
+        val email: EditText = findViewById(R.id.email)
+        val password: EditText = findViewById(R.id.password)
+        val btnRegister: Button = findViewById(R.id.sign_up_button)
+        val alreadyHaveAccount: TextView = findViewById(R.id.already_have_account)
+
+        //Set event listeners
+        btnRegister.setOnClickListener {
+            val bottomSheet = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.term_conditions_bottom_sheet, null)
+            bottomSheet.setContentView(view)
+            bottomSheet.show()
+
+            //On click btnAceptarTerminosCondiciones go to the next activity
+            val btnAceptarTerminosCondiciones: Button = view.findViewById(R.id.accept)
+            val btnCancelarTerminosCondiciones: TextView = view.findViewById(R.id.cancel)
+
+            btnAceptarTerminosCondiciones.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            btnCancelarTerminosCondiciones.setOnClickListener {
+                bottomSheet.dismiss()
+            }
+        }
+
+        alreadyHaveAccount.setOnClickListener {
+            val intent = Intent(this, InicioSesionActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+
+}
