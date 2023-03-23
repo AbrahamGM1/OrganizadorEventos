dc83687ba79a0d9be79575fdc3df1968c056b44f
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2023-03-23 00:08:30 -0700

    Diseño base de eventos y tareas

--- app/src/main/java/gomez/abraham/organizadoreventos/MainActivity.kt
+++ app/src/main/java/gomez/abraham/organizadoreventos/MainActivity.kt
@@ -1,11 +1,21 @@
 package gomez.abraham.organizadoreventos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var boton: Button = findViewById(R.id.boton_prueba)
+
+        boton.setOnClickListener {
+            var intent = Intent(this, EventosActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
