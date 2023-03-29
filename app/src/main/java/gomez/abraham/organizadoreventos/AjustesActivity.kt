c3494ffaf57f8d24b80175458c999ce4ab1891f1
River <75063620+RvrCS@users.noreply.github.com>
2023-03-29 15:26:40 -0700

    Created settingsActivity and layout_settings

---
+++ app/src/main/java/gomez/abraham/organizadoreventos/AjustesActivity.kt
@@ -0,0 +1,11 @@
+package gomez.abraham.organizadoreventos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AjustesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ajustes)
+    }
+}
